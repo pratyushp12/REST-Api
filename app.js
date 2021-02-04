69c6f9ce74cb919b26ec1600b98a36bbc2619e47
Pratyush Praveen <pratyush1296@gmail.com>
2021-02-04 16:21:58 +0530

    Initial commit with common Rest Api Methods

---
+++ app.js
@@ -0,0 +1,112 @@
+const express = require('express');
+const ejs = require('ejs');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static('public'));
+
+mongoose.connect('mongodb://localhost:27017/wikiDB', {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+});
+
+const articleSchema = new mongoose.Schema({
+  title: String,
+  content: String
+});
+
+const Article = mongoose.model("Article", articleSchema);
+
+app.route("/articles")
+  .get((req, res) => {
+    Article.find((err, article) => {
+      if (!err)
+        res.send(article);
+      else
+        console.log(err);
+    })
+  })
+  .post((req, res) => {
+    const newArticle = new Article({
+      title: req.body.title,
+      content: req.body.content
+    });
+    newArticle.save((err) => {
+      if (!err)
+        res.send("Successfully Inserted to the Db");
+      else
+        res.send(err);
+    })
+
+
+  })
+  .delete((req, res) => {
+    Article.deleteMany((err) => {
+      if (!err)
+        res.send("Successfully deleted the documents");
+      else
+        res.send(err);
+    })
+  });
+
+
+app.route("/articles/:articleTitle")
+  .get((req, res) => {
+    const articleTitle = req.params.articleTitle;
+    Article.find({
+      title: articleTitle
+    }, (err, foundArticles) => {
+      if (!err)
+        res.send(foundArticles);
+      else
+        res.send(err);
+    })
+  })
+  .put((req, res) => {
+    Article.update({
+        title: req.params.articleTitle
+      }, {
+        title: req.body.title,
+        content: req.body.content
+      }, {
+        overwrite: true
+      },
+      (err) => {
+        if (!err)
+          res.send("Updated Successfully");
+        else
+          res.send(err);
+      })
+  })
+  .patch((req, res) => {
+    Article.update({
+        title: req.params.articleTitle
+      }, {
+        $set: req.body
+      },
+      (err) => {
+        if (!err)
+          res.send("Updated Successfully");
+        else
+          res.send(err);
+      })
+  })
+  .delete((req,res)=>{
+    Article.deleteOne({title : req.params.articleTitle},
+    (err)=>{
+      if(!err)
+      res.send("One item deleted successfully");
+      else
+      res.send(err);
+    })
+  });
+
+app.listen(3000, () => {
+  console.log("Listening to Port 3000");
+})
